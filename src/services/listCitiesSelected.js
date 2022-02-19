7035c0005330d8ed504a86d1fe1b940ad190c5b3
Markos <markosmk@me.com>
2022-02-18 23:25:45 -0300

    add: list of cities selected for select form

---
+++ src/services/listCitiesSelected.js
@@ -0,0 +1,42 @@
+export const listCitiesSelected = [
+  {
+    id: 1,
+    name: 'San Miguel de Tucuman',
+    state: 'Tucuman',
+    country: 'AR',
+    lat: -26.8303703,
+    lon: -65.2038133,
+  },
+  {
+    id: 2,
+    name: 'Calafate',
+    state: 'Santa Cruz',
+    country: 'AR',
+    lat: -50.3388712,
+    lon: -72.2720843,
+  },
+  {
+    id: 3,
+    name: 'Bariloche',
+    state: 'Rio Negro',
+    country: 'AR',
+    lat: -41.1334421,
+    lon: -71.3098425,
+  },
+  {
+    id: 4,
+    name: 'La Plata',
+    state: 'Buenos Aires',
+    country: 'AR',
+    lat: -34.9206797,
+    lon: -57.9537638,
+  },
+  {
+    id: 5,
+    name: 'Mendoza',
+    state: 'Mendoza',
+    country: 'AR',
+    lat: -32.8894155,
+    lon: -68.8446177,
+  },
+];
