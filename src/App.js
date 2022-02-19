318f69bc19de401b2be1f5b997b1d216334d84d3
Markos <markosmk@me.com>
2022-02-18 23:20:14 -0300

    update: added styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Forecast from './components/Forecast';
+import IconClima from './components/Icons/IconClima';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="max-w-xl mx-auto p-4 sm:px-0">
+      <div className="text-center">
+        <IconClima />
+        <h2 className="mt-2 text-lg font-medium text-black">Clima App</h2>
+        <p className="mt-1 text-sm text-slate-500">
+          La App permite visualizar el pronóstico climático actual y los proximos 5 dias
+          de tu ubicacion. Ademas puedes visualizar el pronóstico de otras 5 ciudades
+          seleccionables.
+        </p>
+      </div>
+      <Forecast />
+    </div>
+  );
 }
 
 export default App;
