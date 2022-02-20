bac468ef91ce7175c07e50363183676b03e50b82
Markos <markosmk@me.com>
2022-02-20 09:44:32 -0300

    update: added parse date utc + timezone offset

--- src/services/getWeatherData.js
+++ src/services/getWeatherData.js
@@ -1,29 +1,36 @@
 import dayjs from 'dayjs';
-import localizedFormat from 'dayjs/plugin/localizedFormat';
+import utc from 'dayjs/plugin/utc';
 import 'dayjs/locale/es';
 dayjs.locale('es');
-dayjs.extend(localizedFormat);
+dayjs.extend(utc);
 
-export const getCurrentInfo = (current, city) => {
+const parseDate = (date, format, offset = '') => {
+  return dayjs
+    .unix(date + offset)
+    .utc()
+    .format(format);
+};
+
+export const getCurrentInfo = (current, timezone_offset, city) => {
   return {
     city: city.name,
     state: city.state,
     country: city.country,
-    today: dayjs().format('LLLL'),
+    today: parseDate(current.dt, 'dddd[,] D [de] MMMM [de] YYYY HH:mm', timezone_offset),
     temperature: current.temp + ' °C',
     feels_like: current.feels_like + ' °C',
-    sunrise: dayjs.unix(current.sunrise).format('LT'),
-    sunset: dayjs.unix(current.sunset).format('LT'),
+    sunrise: parseDate(current.sunrise, 'HH:mm', timezone_offset),
+    sunset: parseDate(current.sunset, 'HH:mm', timezone_offset),
     description: current.weather[0].description.toUpperCase(),
     icon: `https://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`,
   };
 };
 
 // daily get 7 next days + current, with slice we remove 2 last items)
-export const getFiveDays = (daily) => {
+export const getFiveDays = (daily, timezone_offset) => {
   return daily.slice(0, -2).map((item) => {
     return {
-      date: dayjs.unix(item.dt).format('dddd'),
+      date: parseDate(item.dt, 'dddd', timezone_offset), // , // dayjs.unix(item.dt).format('dddd')
       icon: `https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`,
       temperature: item.temp.day + ' °C',
       description: item.weather[0].description,
