aacf94a2b703d1c7632e32319c77e77bd8535a9c
Markos <markosmk@me.com>
2022-02-19 23:04:19 -0300

    add: functions to help to order data in components

---
+++ src/services/getWeatherData.js
@@ -0,0 +1,32 @@
+import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import 'dayjs/locale/es';
+dayjs.locale('es');
+dayjs.extend(localizedFormat);
+
+export const getCurrentInfo = (current, city) => {
+  return {
+    city: city.name,
+    state: city.state,
+    country: city.country,
+    today: dayjs().format('LLLL'),
+    temperature: current.temp + ' °C',
+    feels_like: current.feels_like + ' °C',
+    sunrise: dayjs.unix(current.sunrise).format('LT'),
+    sunset: dayjs.unix(current.sunset).format('LT'),
+    description: current.weather[0].description.toUpperCase(),
+    icon: `http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`,
+  };
+};
+
+// daily get 7 next days + current, with slice we remove 2 last items)
+export const getFiveDays = (daily) => {
+  return daily.slice(0, -2).map((item) => {
+    return {
+      date: dayjs.unix(item.dt).format('dddd'),
+      icon: `http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`,
+      temperature: item.temp.day + ' °C',
+      description: item.weather[0].description,
+    };
+  });
+};
