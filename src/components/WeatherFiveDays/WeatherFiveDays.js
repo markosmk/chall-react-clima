5bda1919b7fbee3edec349cf845bef974d1558d9
Markos <markosmk@me.com>
2022-02-20 01:13:39 -0300

    update: items days flex-wrap responsive

--- src/components/WeatherFiveDays/WeatherFiveDays.js
+++ src/components/WeatherFiveDays/WeatherFiveDays.js
@@ -8,18 +8,18 @@ const WeatherFiveDays = ({ daily }) => {
       </div>
 
       <div className="border-2 border-slate-200 p-4 text-black relative min-w-0 break-words rounded-lg overflow-hidden">
-        <div className="flex gap-2">
+        <div className="flex gap-0 sm:gap-2 flex-wrap sm:flex-nowrap justify-between">
           {daily &&
             daily.map(({ date, temperature, icon, description }, idx) => {
               return (
                 <div
                   key={idx}
-                  className="text-center flex items-center justify-start flex-col w-1/6"
+                  className="text-center mb-4 sm:mb-0 flex items-center justify-start flex-col w-1/3 sm:w-1/6"
                 >
                   <span className="my-1 text-xs sm:text-sm md:text-base capitalize">
                     {idx === 0 ? 'Hoy' : idx === 1 ? 'Mañana' : date}
                   </span>
-                  <img src={icon} className="w-16 h-16" alt="icono clima" />
+                  <img src={icon} className="w-16 h-16" alt="icon weather" />
                   <span className="my-1 text-sm font-semibold">{temperature}</span>
                   <span className="my-1 text-xs text-slate-400">{description}</span>
                 </div>
