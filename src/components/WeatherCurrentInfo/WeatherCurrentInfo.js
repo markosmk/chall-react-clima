1bee6916675bb04f0e3c6bbc37a563d0aefd3e47
Markos <markosmk@me.com>
2022-02-19 22:59:18 -0300

    update: refactor component

---
+++ src/components/WeatherCurrentInfo/WeatherCurrentInfo.js
@@ -0,0 +1,62 @@
+const WeatherCurrentInfo = ({ current }) => {
+  return (
+    <>
+      <div className="my-6">
+        <h3 className="text-sm font-semibold text-slate-500 uppercase tracking-wide">
+          Clima Actual de: {current.city}
+        </h3>
+      </div>
+
+      <div className="border-2 border-slate-200 p-4 text-black relative min-w-0 break-words rounded-lg overflow-hidden mb-4 w-full">
+        <div className="relative">
+          <div className="flex mb-4 justify-between items-center">
+            <div>
+              <h3 className="font-medium text-xl">{current.city}</h3>
+              <h3 className="font-medium text-xl">
+                {current.state}, {current.country}
+              </h3>
+              <h4 className="mb-4 text-slate-500">{current.today}</h4>
+              <small className="text-slate-400">{current.description}</small>
+            </div>
+            <div className="text-right">
+              <img src={current.icon} alt="" className="w-24 h-24 mx-auto" />
+              <h3 className="font-bold text-3xl">{current.temperature}</h3>
+            </div>
+          </div>
+          <div className="block sm:flex justify-between items-center flex-wrap border-t border-slate-100 pt-4">
+            <div className="w-full sm:w-1/2">
+              <div className="flex mb-2 justify-between items-center">
+                <span>Temperatura Actual</span>
+                <small className="sm:px-2 font-semibold text-sm">
+                  {current.temperature}
+                </small>
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2">
+              <div className="flex mb-2 justify-between items-center">
+                <span>Sensacion</span>
+                <small className="sm:px-2 font-semibold text-sm">
+                  {current.feels_like}
+                </small>
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2">
+              <div className="flex mb-2 justify-between items-center">
+                <span>Amanecer</span>
+                <small className="sm:px-2 font-semibold text-sm">{current.sunrise}</small>
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2">
+              <div className="flex mb-2 justify-between items-center">
+                <span>Atardecer</span>
+                <small className="sm:px-2 font-semibold text-sm">{current.sunset}</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WeatherCurrentInfo;
