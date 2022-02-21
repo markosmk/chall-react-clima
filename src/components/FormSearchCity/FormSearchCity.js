156718a126d95979105226516ea52de460484a76
Markos <markosmk@me.com>
2022-02-20 21:38:43 -0300

    update: add transition tailwindcss

--- src/components/FormSearchCity/FormSearchCity.js
+++ src/components/FormSearchCity/FormSearchCity.js
@@ -22,12 +22,12 @@ const FormSearch = ({ handleSubmit }) => {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         required
-        className="focus:ring-0 focus:border-sky-500 block w-full sm:text-sm border-2 border-slate-200 rounded-md"
+        className="focus:ring-0 focus:border-sky-500 block w-full sm:text-sm border-2 border-slate-200 rounded-md transition-colors"
         placeholder="Ciudad, Cod Pais (ej: Neuquen, AR )"
       />
       <button
         type="submit"
-        className="ml-4 flex-shrink-0 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-sky-500 hover:bg-sky-600 focus:outline-none focus:ring-0 active:bg-sky-800 w-14 sm:w-auto"
+        className="ml-4 flex-shrink-0 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-sky-500 hover:bg-sky-600 focus:outline-none focus:ring-0 active:bg-sky-800 w-14 sm:w-auto transition-colors"
       >
         <IconSearch className="h-6 w-6 sm:h-4 sm:w-4 inline-flex" />
         <span className="hidden sm:inline-flex sm:ml-2">Buscar</span>
